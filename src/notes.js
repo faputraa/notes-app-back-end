cb985d4b0a41beccd297f6e60096cca23373604e
faputraa <fatrahputra.18@gmail.com>
2023-10-05 18:17:18 +0700

    initial commit

---
+++ src/notes.js
@@ -0,0 +1,5 @@
+// Webnya => http://notesapp-v1.dicodingacademy.com/
+// Memuat data notes yang disimpan dalam bentuk array objek.
+const notes = [];
+ 
+module.exports = notes;
---
+++ src/server.js
@@ -0,0 +1,19 @@
+// Webnya => http://notesapp-v1.dicodingacademy.com/
+// Memuat kode untuk membuat, mengonfigurasi, dan menjalankan HTTP server menggunakan Hapi.
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+ 
+const init = async () => {
+    const server = Hapi.server({
+        port: 5000,
+        host: 'localhost',
+        routes: {cors:{origin:['*'],},},
+    });
+ 
+    server.route(routes);
+
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+};
+ 
+init();
